8b87d555326220322e42bfa8705ff3f33c875441
meisteregor <33342704+meisteregor@users.noreply.github.com>
2020-12-04 04:56:07 +0300

    Add files via upload

---
+++ return_threads_values.py
@@ -0,0 +1,24 @@
+import threading
+import random
+import time
+
+
+def thread_job(results, index):
+    time.sleep(1)
+    results[index] = random.randint(1, 5)
+
+
+def gather(threads_count):
+    threads = [None] * threads_count
+    results = [None] * threads_count
+    for i in range(threads_count):
+        threads[i] = threading.Thread(target=thread_job, args=(results, i))
+        threads[i].start()
+    # do some other stuff
+    for i in range(threads_count):
+        threads[i].join()
+    return results
+
+
+x = gather(random.randint(1, 9))
+print(x)
