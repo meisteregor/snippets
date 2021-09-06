76470e9bffb67ed311cf1f0faea20d5eb5b9a406
meisteregor <33342704+meisteregor@users.noreply.github.com>
2021-09-06 09:19:10 +0300

    Update custom_logger_new.py

--- custom_logger_new.py
+++ custom_logger_new.py
@@ -4,7 +4,7 @@ import urllib3
 
 # minimal
 def set_logger():
-    logfile = os.path.dirname(os.path.abspath(__file__)) + "/" + cons.LOGFILE
+    logfile = os.path.dirname(os.path.abspath(__file__)) + os.sep + cons.LOGFILE
     logger.add(logfile, format="{time} {level} {message}", level="DEBUG", rotation="1 year",
                compression="zip")
 
@@ -14,7 +14,7 @@ def setup_logger():
     LOG_FORMAT_MINIMAL = "[<level>{level: <8}</level>] <level>{message}</level>"
     LOG_FORMAT_SCREEN = LOG_FORMAT_MINIMAL
     LOG_FORMAT_FILE = TIMESTAMP_STR + " " + LOG_FORMAT_MINIMAL
-    LOG_FILE = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/" + 'logs/' + os.path.basename(__file__).replace('.py', '.log')
+    LOG_FILE = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + os.sep + 'logs' + os.sep + os.path.basename(__file__).replace('.py', '.log')
     LOG_CONFIG = {
         "handlers": [
             {"sink": sys.stdout, "format": LOG_FORMAT_SCREEN, "level": "WARNING"},
